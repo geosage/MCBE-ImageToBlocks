689effb9bc96edf6e0ffbd862dd8773a732b1114
Your Name <georgecp2006@gmail.com>
2023-03-11 12:50:20 +0000

    Reworked Upload

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from imageprocessing import *
 from blockpicker import *
 import numpy as np
+
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image
+
+#Image Upload ------------------------------
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.png;*.gif;*.bmp")])
+
+if file_path:
+    with Image.open(file_path) as img:
+            img.save(f"mainimage.png")
+            print("Successfully uploaded image")
+else:
+    print("No file selected")
+#------------------------------------------
+
 maxheight = 320
 q = 1
 pixelsperblock = resize_and_save_image(maxheight)
